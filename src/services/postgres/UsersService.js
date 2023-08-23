298916c7861158b471d31b648a86fec66e694f96
SyahranFD <fadhilrafa1@gmail.com>
2023-08-23 23:50:14 +0700

    server-side caching implemented

--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,7 +3,7 @@ const { nanoid } = require('nanoid');
 const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
-const NotFoundError = require('../../exceptions/NotfoundError');
+const NotFoundError = require('../../exceptions/NotFoundError');
 const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
